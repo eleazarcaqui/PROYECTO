6c280a80ee031a51f335ad94895bb71f1ec291db
Eleazar Caqui <eleazar969mejia@gmail.com>
2023-07-10 19:53:36 -0500

    modificacion de road map mas eficiente

--- js/roadmap2.js
+++ js/roadmap2.js
@@ -1,55 +1,77 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // Agregar la clase 'active' al primer título
+  titles[0].classList.add('active');
+  const firstContent = titles[0].nextElementSibling;
+  firstContent.classList.add('active');
+  title2[0].classList.add('active');
+});
 
 const title2 = document.querySelectorAll('.timeline-title');
+const icons = document.querySelectorAll('.timeline-title .bi.bi-chevron-compact-down');
+const titles = document.querySelectorAll('.timeline-title .title');
+const contents = document.querySelectorAll('.timeline-title .content');
 
-title2.forEach(title => {
-    title.addEventListener('click', () => {
-        // Remover la clase 'active' de todos los elementos de título
-        title2.forEach(item => {
-            item.classList.remove('active');
-        });
-
-        // Agregar la clase 'active' solo al elemento de título actual
-        title.classList.add('active');
+title2.forEach((title, index) => {
+  title.addEventListener('click', () => {
+    // Remover la clase 'active' de todos los elementos de título
+    title2.forEach(item => {
+      item.classList.remove('active');
     });
-});
-// Obtener todos los elementos de título
-const titles = document.querySelectorAll('#timeline .title');
 
-function toggleAccordion() {
-    const accordion = document.getElementById('accordion');
-    accordion.classList.toggle('active');
-  }
+    // Agregar la clase 'active' solo al elemento de título actual
+    title.classList.add('active');
 
-  document.querySelectorAll('#temario .lista h2').forEach((elemento) => {
-	elemento.addEventListener('click', () => {
-		document.querySelector('#temario .active').classList.remove('active');
-		elemento.parentElement.classList.add('active');
-	});
+    // Cerrar todos los demás elementos de acordeón
+    titles.forEach((t, i) => {
+      if (i !== index) {
+        const content = t.nextElementSibling;
+        content.classList.remove('active');
+        t.classList.remove('active');
+      }
+    });
+  });
 });
+
+icons.forEach((icon, index) => {
+  icon.addEventListener('click', () => {
+    const content = contents[index];
+    const isActive = content.classList.contains('active');
+
+    // Mostrar/ocultar contenido al hacer clic en el icono
+    contents.forEach(c => {
+      c.classList.remove('active');
+    });
+
+    // Remover la clase 'active' de todos los elementos de título
+    titles.forEach(title => {
+      title.classList.remove('active');
+    });
+
+    if (!isActive) {
+      content.classList.add('active');
+      titles[index].classList.add('active');
+    }
+  });
+});
+const titles3 = document.querySelectorAll('#timeline .title');
+
   // Agregar evento de clic a cada elemento de título
-  titles.forEach((title, index) => {
+  titles3.forEach((title, index) => {
     title.addEventListener('click', () => {
-      // Obtener el elemento de contenido correspondiente al título
-      const content = title.nextElementSibling;
-  
-      // Mostrar/ocultar contenido al hacer clic en el título
-      content.classList.toggle('active');
-  
-     // Remover la clase 'active' de todos los elementos de título
-      titles.forEach((t, i) => {
+      titles3.forEach((t, i) => {
         if (i !== index) {
           t.classList.remove('active');
         }
       });
-  
       // Agregar la clase 'active' al elemento de título actual
       title.classList.add('active');
     });
   });
+
   function image1() {
     document.getElementById("image1").style.display = "block"
     document.getElementById("image2").style.display = "none"
-    document.getElementById("image4").style.display = "none"
+    document.getElementById("image3").style.display = "none"
     document.getElementById("image4").style.display = "none"
     document.getElementById("image5").style.display = "none"
     document.getElementById("image6").style.display = "none"
